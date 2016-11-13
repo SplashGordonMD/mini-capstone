f6fcabf5708f4d4dbb5bd72ae90754c2ec322c3b
Casey Platt <kse0824@gmail.com>
2016-11-13 14:08:52 -0600

    mini capstone

--- app/controllers/lemonaids_controller.rb
+++ app/controllers/lemonaids_controller.rb
@@ -1,7 +1,34 @@
 class LemonaidsController < ApplicationController
+  
   def index
     @lemonaid = Lemonaid.all
     render "lemonaid_stand_offerings.html.erb"
   end 
 
+  def new
+    render 'new.html.erb'
+  end
+
+  def show
+    lemonaid_id = params[:id]
+    @lemonaid = Lemonaid.find_by(id: lemonaid_id)
+    render "show.html.erb" 
+  end
+
+  def edit
+    lemonaid_id = params[:id]
+    @lemonaid = Lemonaid.find_by(id: lemonaid_id)
+  end
+
+  def update  
+    recipe.save
+    render 'update.html.erb'
+  end
+
+  def destroy
+    lemonaid_id = params[:id]
+    lemonaid = Lemonaid.find_by(id: lemonaid_id)
+    lemonaid.destroy
+    render 'destroy.html.erb'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/lemonaid_stand' => 'lemonaids#index'
-  # get '/'
+  get '/lemonaids' => 'lemonaids#index'
+  get '/lemonaids/new' => 'lemonaids#new'
+  post '/lemonaids' => 'lemonaids#create'
+  get '/lemonaids/:id' => 'lemonaids#show' 
+  get '/lemonaids/:id/edit' => 'lemonaids#edit'
+  patch '/lemonaids/:id' => 'lemonaids#update'
+  delete '/lemonaids/:id' => 'lemonaids#destroy'
 end
+
+  
 
